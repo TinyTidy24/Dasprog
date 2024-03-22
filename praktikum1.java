b3021a484a74ae0b0e4ef65834aa048bb8f6d392
TinyTidy24 <162095912+TinyTidy24@users.noreply.github.com>
2024-03-22 13:39:57 +0700

    Add files via upload

---
+++ praktikum1.java
@@ -0,0 +1,14 @@
+package dasprog;
+public class praktikum1 {
+    public static void main(String[] args){
+        System.out.println
+        ("  /\\\\_//\\\n" 
+        + " / 0   0 \\\n"
+        + "/_   ^   _\\\n"
+        + "  \\_____/\n"
+        + "  /  0  \\\n"
+        + " /       \\_\n"
+        + " \\_(_|_)__ \\\n"
+        + "        (__/");
+    }
+}
